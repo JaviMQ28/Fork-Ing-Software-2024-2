ce754f8236c3226aed7397e78376e9aba0d916ac
Javier Mancera <javiermancera@2806-106e-0005-6b17-87b0-8626-a4ed-3336.ipv6.infinitum.net.mx>
2024-02-15 23:48:41 -0600

    Script2 final

--- Script2.py
+++ Script2.py
@@ -20,7 +20,7 @@ def contarValles(cadena):
         elif i == 'U' and altura == 0:
             numValle += 1
             bandera = False
-    print(f'-> Cantidad de valles que se recorrieron = {numValle} valle(s)')
+    print(f' -> Cantidad de valles que se recorrieron = {numValle} valle(s)\n')
 
 # ----------------------------------------------
 class Nodo:
@@ -71,20 +71,27 @@ class Nodo:
     # Izquierdo, raiz, derecho
     def inorden(self):         
 
+        agregado = False
         if self.izq is None and self.der is None:
             print(self.elemento, end="")          
+            agregado = True
         elif self.izq is not None:
             self.izq.inorden()
         if self.der is not None:
             print(end=" , ")
             print(self.elemento, end=" , ")            
-            self.der.inorden()
+            self.der.inorden()        
+        elif self.der is None and not(agregado):
+            print(end=" , ")
+            print(self.elemento, end="")            
 
     # Izquierdo, derecho, raiz
     def postorden(self):         
 
+        agregado = False
         if self.izq is None and self.der is None:
-            print(self.elemento, end="")          
+            print(self.elemento, end="")      
+            agregado = True    
         elif self.izq is not None:
             self.izq.postorden()
             print(end=" , ")
@@ -92,104 +99,107 @@ class Nodo:
             self.der.postorden()    
             print(end=" , ")        
             print(self.elemento, end="")
+        elif self.der is None and not(agregado):
+            print(self.elemento, end="")
 
+if __name__ == '__main__':
 
-# Ejercicio seleccionado
-ejercicioSel = 0
-while ejercicioSel != 3:
-    try:
-        ejercicioSel = int(input("- Selecciona algun ejercicio para probarlo, ingresando el número que lo identifica: \n   1. Recorrido de un caminante. \n   2. Árbol binario. \n   3. Salir.\n"))
-        if ejercicioSel < 1 or ejercicioSel > 3:
+    # Ejercicio seleccionado
+    ejercicioSel = 0
+    while ejercicioSel != 3:
+        try:
+            ejercicioSel = int(input("- Selecciona algun ejercicio para probarlo, ingresando el número que lo identifica: \n   1. Recorrido de un caminante. \n   2. Árbol binario. \n   3. Salir.\n"))
+            if ejercicioSel < 1 or ejercicioSel > 3:
+                print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2,3).")        
+        except:
             print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2,3).")        
-    except:
-        print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2,3).")        
-        ejercicioSel = 0 
+            ejercicioSel = 0 
 
-    # Recorrido del caminante.
-    if ejercicioSel == 1:
-        res = 0
-        while res != 2:
-            # Sirve para saber si la cadena es correcta, es decir que solo contiene las letras U y D
-            cadenaCorrecta = False
-            # Cantidad de letras o otros caracteres especiales que no son U ni D.
-            numError = 0
+        # Recorrido del caminante.
+        if ejercicioSel == 1:
+            res = 0
+            while res != 2:
+                # Sirve para saber si la cadena es correcta, es decir que solo contiene las letras U y D
+                cadenaCorrecta = False
+                # Cantidad de letras o otros caracteres especiales que no son U ni D.
+                numError = 0
 
-            while not(cadenaCorrecta):
-                cadena = input("\nDame una cadena, que solo contenga las letras U y D:\n")    
+                while not(cadenaCorrecta):
+                    cadena = input("\nDame una cadena, que solo contenga las letras U y D:\n")    
 
-                if cadena == "":
-                    cadenaCorrecta = True
-                else:
-                    # Verifica que la cadena recibida sea con el formato correcto
-                    for i in cadena:
-                        if i != 'D' and i != 'U':
-                            numError += 1
-                        
-                        if numError == 0:
-                            cadenaCorrecta = True
+                    if cadena == "":
+                        cadenaCorrecta = True
+                    else:
+                        # Verifica que la cadena recibida sea con el formato correcto
+                        for i in cadena:
+                            if i != 'D' and i != 'U':
+                                numError += 1
+                            
+                            if numError == 0:
+                                cadenaCorrecta = True
+                            else:
+                                cadenaCorrecta = False
+                    
+                    if not(cadenaCorrecta):
+                        print("ERROR: La cadena de caracteres debe de contener las letras U y D, intentalo nuevamente.")
+                        numError = 0    
+
+                # Resultado de la cantidad de valles que tiene
+                contarValles(cadena)
+
+                selec = False
+
+                while not(selec):
+                    try:
+                        res = int(input("- Selecciona una opción, ingresando el número que lo identifica: \n   1. Volver a ejecutar el programa. \n   2. Salir.\n"))
+                        if res >= 1 and res <= 2:
+                            selec = True
                         else:
-                            cadenaCorrecta = False
-                
-                if not(cadenaCorrecta):
-                    print("ERROR: La cadena de caracteres debe de contener las letras U y D, intentalo nuevamente.")
-                    numError = 0    
-
-            # Resultado de la cantidad de valles que tiene
-            contarValles(cadena)
-
-            selec = False
-
-            while not(selec):
-                try:
-                    res = int(input("- Selecciona una opción, ingresando el número que lo identifica: \n   1. Volver a ejecutar el programa. \n   2. Salir.\n"))
-                    if res >= 1 and res <= 2:
-                        selec = True
-                    else:
+                            print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2).")        
+                    except:
                         print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2).")        
-                except:
-                    print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2).")        
-                    res = None        
+                        res = None        
 
-    # Arbol binario
-    elif ejercicioSel == 2:
-        raizAgregado = False
-        while not(raizAgregado):
-            elem = input("Ingresa un elemento: ")
-            if elem != "":
-                raizAgregado = True
-            else:
-                print("ERROR: No ingresaste ningun elemento, intentalo nuevamente.")
-        raiz = Nodo(elem, None, None, None)
-        res = 0
-        while res != 5:
-            selec = False
+        # Arbol binario
+        elif ejercicioSel == 2:
+            raizAgregado = False
+            while not(raizAgregado):
+                elem = input("Ingresa un elemento: ")
+                if elem != "":
+                    raizAgregado = True
+                else:
+                    print("ERROR: No ingresaste ningun elemento, intentalo nuevamente.")
+            raiz = Nodo(elem, None, None, None)
+            res = 0
+            while res != 5:
+                selec = False
 
-            print(f"\n -> Árbol = {raiz} \n")
+                print(f"\n -> Árbol = {raiz} \n")
 
-            while not(selec):
-                try:
-                    res = int(input("- Selecciona una opción, ingresando el número que lo identifica: \n   1. Agregar un elemento. \n   2. Recorrido pre-orden. \n   3. Recorrido in-orden. \n   4. Recorrido post-orden. \n   5. Salir.\n"))
-                    if res >= 1 and res <= 5:
-                        selec = True
-                    else:
+                while not(selec):
+                    try:
+                        res = int(input("- Selecciona una opción, ingresando el número que lo identifica: \n   1. Agregar un elemento. \n   2. Recorrido pre-orden. \n   3. Recorrido in-orden. \n   4. Recorrido post-orden. \n   5. Salir.\n"))
+                        if res >= 1 and res <= 5:
+                            selec = True
+                        else:
+                            print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2,3,4,5).")        
+                    except:
                         print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2,3,4,5).")        
-                except:
-                    print("ERROR: La selección no es correcta, ingresa alguno de los números que se muestran (1,2,3,4,5).")        
-                    res = None
+                        res = None
 
-            match res:
-                case 1:
-                    elemAgrega = input("Ingresa el elemento que se quiera agregar al árbol: ")
-                    raiz.agregaElemento(elemAgrega)
-                case 2:
-                    print(end="\nRecorrido pre-orden = [")
-                    raiz.preorden()
-                    print(end="]")
-                case 3:
-                    print(end="\nRecorrido in-orden = [")
-                    raiz.inorden()
-                    print(end="]")
-                case 4:
-                    print(end="\nRecorrido post-orden = [")
-                    raiz.postorden()
-                    print(end="]")
+                match res:
+                    case 1:
+                        elemAgrega = input("Ingresa el elemento que se quiera agregar al árbol: ")
+                        raiz.agregaElemento(elemAgrega)
+                    case 2:
+                        print(end="\nRecorrido pre-orden = [")
+                        raiz.preorden()
+                        print(end="]")
+                    case 3:
+                        print(end="\nRecorrido in-orden = [")
+                        raiz.inorden()
+                        print(end="]")
+                    case 4:
+                        print(end="\nRecorrido post-orden = [")
+                        raiz.postorden()
+                        print(end="]")
