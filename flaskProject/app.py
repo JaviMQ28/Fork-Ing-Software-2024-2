40ec33cff76218173811c8cbcfd2b0947989b55b
JaviMQ28 <javier_manceraq@ciencias.unam.mx>
2024-03-18 22:45:09 -0600

    Subiendo app.py

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template
 
 from alchemyClasses import db
-from contollers.PrimerControlador import mi_primer_blueprint
-from contollers.ControllerAlumno import alumno_blueprint
+from contollers.MenuControlador import menu_blueprint
+from contollers.UsuarioControlador import usuario_blueprint
+from contollers.PeliculaControlador import pelicula_blueprint
+from contollers.RentarControlador import rentar_blueprint
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Develooper123!@localhost:3306/ing_soft'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_software'
 app.config.from_mapping(
     SECRET_KEY='dev'
 )
 db.init_app(app)
-app.register_blueprint(mi_primer_blueprint)
-app.register_blueprint(alumno_blueprint)
+app.register_blueprint(menu_blueprint)
+app.register_blueprint(usuario_blueprint)
+app.register_blueprint(pelicula_blueprint)
+app.register_blueprint(rentar_blueprint)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html')
+    return render_template('menu.html')
 
 if __name__ == '__main__':
     app.run()
